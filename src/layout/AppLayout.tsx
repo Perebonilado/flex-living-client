8f9950bc40d4aea1e7ca072a7ea0401aec6bb378
perebonilado <eradiribonnie@gmail.com>
2025-09-21 20:03:13 +0100

    head

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,10 +1,14 @@
 import Container from "@/components/shared/Container";
 import Navbar from "@/components/shared/Navbar";
+import Head from "next/head";
 import React, { FC, PropsWithChildren } from "react";
 
 const AppLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
+    <Head>
+      <title>The Flex</title>
+    </Head>
       <Navbar />
       <main>
         <Container>{children}</Container>
